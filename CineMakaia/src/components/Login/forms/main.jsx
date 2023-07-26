bfc4eb6ac0e6aa9e0ede2f174338e5630ec9b6d8
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-07-26 08:31:52 -0500

    mend

---
+++ CineMakaia/src/components/Login/forms/main.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import "./main.scss"
+function FormLogin() {
+  
+  
+  
+  
+  
+    return (
+    <>
+    <div class="login" id="loginForm">
+     <div class="login_content">
+      <span class="login_close">&times;</span>
+      <h2>Login</h2>
+      <p>Contenido del modal...</p>
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default FormLogin
--- CineMakaia/src/components/home/main.jsx
+++ CineMakaia/src/components/home/main.jsx
@@ -4,12 +4,14 @@ import Header from '../header/main.jsx'
 import "./main.scss"
 import BodyHome from './body/main.jsx';
 import { Outlet, useParams } from 'react-router-dom';
+import FormLogin from '../Login/forms/main.jsx';
 
 
 function  Home() {
   return(
       <>
       <Header isHome={true}/>
+      <FormLogin/>
       <Outlet/>
       </>
     )

357950db6aa6b51b7c7ba5d5c45e43a4b4c01a16
José Henao <josexdjose14@gmail.com>
2023-08-08 00:00:42 -0500

    click on selectFilm feature fixed

--- CineMakaia/src/hooks/useGetMovies.js
+++ CineMakaia/src/hooks/useGetMovies.js
@@ -1,23 +1,23 @@
 import { useEffect } from "react";
 import { getMovieFunctions } from "../services/createMovieFunctions";
 
-export const useGetMovie =  (idJSONMovie, date, setMovieFunctions, staticState) => {
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const movieFunctions = await getMovieFunctions(idJSONMovie, date);
-                setMovieFunctions(movieFunctions);
-           
-            } catch(error) {
+export const useGetMovie = (idJSONMovie, date, setMovieFunctions, staticState) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const movieFunctions = await getMovieFunctions(idJSONMovie, date);
+        setMovieFunctions(movieFunctions);
 
-                console.log(error);
-                return error;
-            }
-        };
+      } catch (error) {
 
-        if (date) {
-            fetchData();
-        }
-    }, [date, staticState]);
+        console.log(error);
+        return error;
+      }
+    };
+
+    if (date) {
+      fetchData();
+    }
+  }, [date, staticState, idJSONMovie]);
 };
 
